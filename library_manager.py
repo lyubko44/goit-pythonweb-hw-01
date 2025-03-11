d0569271855b2942f328dd6363bf02d6f91b1eea
Liubomyr Skorokhod <lyubko44@gmail.com>
2025-03-11 00:34:18 -0700

    homework task 2

---
+++ library_manager.py
@@ -0,0 +1,55 @@
+from abc import ABC, abstractmethod
+
+
+class Book:
+    def __init__(self, title: str, author: str, year: str) -> None:
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self) -> str:
+        return f"Title: {self.title}, Author: {self.author}, Year: {self.year}"
+
+
+class LibraryInterface(ABC):
+    @abstractmethod
+    def add_book(self, book: Book) -> None:
+        pass
+
+    @abstractmethod
+    def remove_book(self, title: str) -> None:
+        pass
+
+    @abstractmethod
+    def show_books(self) -> None:
+        pass
+
+
+class Library(LibraryInterface):
+    def __init__(self) -> None:
+        self.books = []
+
+    def add_book(self, book: Book) -> None:
+        self.books.append(book)
+
+    def remove_book(self, title: str) -> None:
+        self.books = [book for book in self.books if book.title != title]
+
+    def show_books(self) -> None:
+        for book in self.books:
+            print(book)
+
+
+class LibraryManager:
+    def __init__(self, library: LibraryInterface) -> None:
+        self.library = library
+
+    def add_book(self, title: str, author: str, year: str) -> None:
+        book = Book(title, author, year)
+        self.library.add_book(book)
+
+    def remove_book(self, title: str) -> None:
+        self.library.remove_book(title)
+
+    def show_books(self) -> None:
+        self.library.show_books()
